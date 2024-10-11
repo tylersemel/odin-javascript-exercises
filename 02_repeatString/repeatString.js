3c2480fe25c50e93d279074f8c5bb3283e89e956
tylersemel <tylersemel@gmail.com>
2024-10-11 18:34:47 -0400

    Repeat a string a number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num === null || num < 0) {
+        return "ERROR";
+    }
+    
+    let repeatedStr = "";
+    for (let i = 0; i < num; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
