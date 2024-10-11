afb81210d75924f6e57b68e90358d6f5410157ef
tylersemel <tylersemel@gmail.com>
2024-10-11 18:54:29 -0400

    Reverse any given string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,25 @@
-const reverseString = function() {
+// const reverseString = function(string) {
+//     let reversedString = "";
 
+//     for (let i = string.length - 1; i >= 0; i--) {
+//         reversedString += string[i];
+//     }
+
+//     return reversedString;
+// };
+
+const reverseString = function(string) {
+    let reversed = string.split('');
+    reversed.reverse();
+    string = "";
+
+    for (const letter of reversed) {
+        string += letter;
+    }
+
+    //or just do return string.split('').reverse().join('');
+
+    return string;
 };
 
 // Do not edit below this line
