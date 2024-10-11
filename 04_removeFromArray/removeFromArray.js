087b7c83f1af105fdb90c7022dc0ae253f065ad7
tylersemel <tylersemel@gmail.com>
2024-10-11 19:23:56 -0400

    Remove any number of items from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, ...itemsToRemove) {
+    for (const item of itemsToRemove) {
+        let foundIndex = numbers.indexOf(item);
+
+        while (foundIndex != -1) {
+            numbers.splice(foundIndex, 1);
+
+            foundIndex = numbers.indexOf(item);
+        }
+    }
+
+    return numbers;
 };
 
 // Do not edit below this line
