5f80a9b42651f31a2d1f0972ab657dee1ff915c5
tylersemel <tylersemel@gmail.com>
2024-10-14 15:28:03 -0400

    Calculate if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //if can be / 4 and is not / 100 unless / 400
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
 
+    if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)) {
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
