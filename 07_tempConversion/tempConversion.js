3253656d87a005bef33d2e0695674315dd9e7977
tylersemel <tylersemel@gmail.com>
2024-10-14 15:40:17 -0400

    Convert celsius to fahrenheit and back

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+function roundToOneDecimal(number) {
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  //formula to convert fahrenheit to celsius
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  let celsiusRounded = roundToOneDecimal(celsius);
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //formula to convert celsius to fahrenheit
+  let fahrenheit = (celsius * (9 / 5) + 32);
+  let fahrenheitRounded = roundToOneDecimal(fahrenheit);
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
