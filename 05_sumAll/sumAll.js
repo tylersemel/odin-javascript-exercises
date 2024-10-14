db285d8a81158007ef440107f773489b9decac31
tylersemel <tylersemel@gmail.com>
2024-10-14 14:34:32 -0400

    Return the sum of every number between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== "number" || typeof end !== "number" ||
+        Math.floor(start) != start || Math.floor(end) != end ||
+        start < 0 || end < 0 ) {
+        return "ERROR";
+    }
 
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
