b07b38ff0b14bf537688d35a43ba203ee78309bc
tylersemel <tylersemel@gmail.com>
2024-10-23 16:58:35 -0400

    Find the oldest person in the array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //sort people by age
+    //retrieve oldest person at the back of the array
+
+    return people.sort((a, b) => {
+        let ageA = 0;
+        let ageB = 0;
+        if (!a.hasOwnProperty("yearOfDeath")) {
+            ageA = new Date().getFullYear() - a.yearOfBirth
+        }
+        else {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
+        console.log(b.hasOwnProperty("yearOfDeath"));
+        if (!b.hasOwnProperty("yearOfDeath")) {
+            ageB = new Date().getFullYear() - b.yearOfBirth
+        }
+        else {
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        return ageA > ageB ? 1 : -1;
+    })[people.length - 1];
+    
 
 };
 
