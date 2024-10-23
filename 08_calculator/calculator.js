cb91c1b6be64fcb5b59384e5f83aadb5feca1daf
tylersemel <tylersemel@gmail.com>
2024-10-23 14:41:13 -0400

    Create a basic calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	// return Math.pow(a, b);
+  let total = 1;
+  for (let i = 0; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+
+  let total = num;
+
+  for (let i = num - 1; i > 0; i--) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
