df8833695c2598ce8eefaf34188bdc84cb2d53b3
tylersemel <tylersemel@gmail.com>
2024-10-23 15:13:53 -0400

    Test palindromes the bad way

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let str = "";
+    let reverse = "";
 
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] !== " " &&
+            ((word[i].toLowerCase() >= "a" && word[i].toLowerCase() <= "z") ||
+            (word[i] >= 0 && word[i].toLowerCase() <= 9))) {
+            str += word[i];
+        }
+    }
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverse += str[i];
+    }
+
+    if (str.toLowerCase() !== reverse.toLowerCase()) {
+        return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
