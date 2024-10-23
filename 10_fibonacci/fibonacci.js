d488fd477d7a690f02fc1db2f2f3cb58e72ccf90
tylersemel <tylersemel@gmail.com>
2024-10-23 16:35:16 -0400

    Calculate fibonacci number in the sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    // 1 1 2 3 5 8
+    // 0 1 2 3 4 5
+    let idx = parseInt(index);
+    if (idx < 0) {
+        return "OOPS";
+    }
 
+    if (idx == 1 || idx == 2) {
+        return 1;
+    }
+    else if (idx == 0) {
+        return 0;
+    }
+
+    let prev1 = 1;
+    let prev2 = 1;
+    let num = prev1 + prev2; //2
+
+    for (let i = 2; i < idx; i++) {
+        num = prev1 + prev2;
+
+        prev1 = prev2;
+        prev2 = num;
+    }
+
+
+    return num;
 };
 
 // Do not edit below this line
